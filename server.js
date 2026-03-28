25083e6d11193d419f99b13e69a7f7270c0d667c
DEILSON   MENDES <deilsongilmar@gmail.com>
2026-03-28 13:40:29 +0000

    app.get

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require('express');
 
 const app = express();
 
+app.get('/', (req, res) => {
+    console.log('Requisição chegou!');
+});
+
 app.listen(1234, () => {
     console.log("Servidor iniciado na porta 1234");
 });
