abcfa730a2abc88db6abeb13516f5331133780f2
DEILSON   MENDES <deilsongilmar@gmail.com>
2026-04-18 11:38:16 +0000

    server.js

--- server.js
+++ server.js
@@ -12,42 +12,6 @@ app.get('/home', (req, res) => {
     res.send("Home Page");
 });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 app.get('/me', (req, res) => {
     const {nome, bairro} = req.query;
     res.send(`Meu nome é ${nome}, Moro em ${bairro}`);
